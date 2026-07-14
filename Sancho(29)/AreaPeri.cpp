af37e5251ec2e842ff36343dabb65e0ca073112d
Sanchit Thanekar <sanchitthanekar5909@gmail.com>
2026-07-14 11:03:17 +0530

    Add files via upload

---
+++ Sancho(29)/AreaPeri.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int length,width,area,perimeter;
+  length=10;
+  width=5;
+  area=length*width;
+  perimeter=2*(length+width);
+  cout<<area<<perimeter;
+}
---
+++ Sancho(29)/Arithematic.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int a,b,sum,sub,mult,div,mod;
+  a=25;
+  b=10;
+  sum=a+b;
+  sub=a-b;
+  mult=a*b;
+  div=a/b;
+  mod=a%b;
+  cout<<sum<<sub<<mult<<div<<mod;
+}
