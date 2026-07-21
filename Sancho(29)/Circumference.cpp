f0fdcfebfd326ce1be7d3de8aca6d8768620c78c
Sanchit Thanekar <sanchitthanekar5909@gmail.com>
2026-07-21 04:54:28 +0000

    Add Circumference calculation program in C++

---
+++ Sancho(29)/Circumference.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namestring std;
+int main()
+{
+    float radius,area,circumference;
+    
+    cout << "Enter radius";
+    cin >> radius;
+
+    area=3.14*radius*radius;
+    circumference=2*3.14*radius;
+
+    cout << "Area=" <<area <<endl;
+    cout << "Circumference=" << circumference <<endl;
+
+    return 0;
+}
